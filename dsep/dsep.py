d163a9daa7d136bace9216daf5351c2350eee031
kkew3 <kps6326@hotmail.com>
2024-10-12 11:52:35 +0800

    Add cli for d-sep of bn

--- dsep/dsep.py
+++ dsep/dsep.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+import typing as ty
 
-import pyAgrum as gum
+import networkx as nx
 import click
+
+from dsep import spec_parser
+from dsep import bn
 
 
 def read_spec(path_or_spec: str):
@@ -9,20 +13,11 @@ def read_spec(path_or_spec: str):
             if Path(path_or_spec).is_file() else path_or_spec)
 
 
-@click.command(
-    help=('Test conditional independence between '
-          'NODE_A and NODE_B given EVIDENCE in a Bayesian Network or '
-          'a Markov Random Field. Print "yes" if they are conditionally '
-          'independent, "no" otherwise.'))
-@click.option(
-    '-g',
-    '--graph-type',
-    'graph_type',
-    type=click.Choice(['bn', 'mrf']),
-    required=True,
-    help=('Type of graph to use: Bayesian Network (bn) or '
-          'Markov Random Field (mrf).'),
-)
+def pprint_nodes(nodes: ty.Iterable[str]) -> str:
+    return '(' + ','.join(nodes) + ')'
+
+
+@click.command()
 @click.option(
     '-i',
     '--spec',
@@ -32,30 +27,64 @@ def read_spec(path_or_spec: str):
     help='Either a path to the graph spec, or the spec string.')
 @click.option(
     '-a',
-    'node_a',
-    metavar='NODE_A',
+    'nodes_a',
+    metavar='NODES_A',
     required=True,
-    help='The node A to test conditional independence.')
+    help=('The node set A to test conditional independence, '
+          'separated by commas. Must be nonempty.'))
 @click.option(
     '-b',
-    'node_b',
-    metavar='NODE_B',
+    'nodes_b',
+    metavar='NODES_B',
     required=True,
-    help='The node B to test conditional independence.')
+    help=('The node set B to test conditional independence, '
+          'separated by commas. Must be nonempty.'))
 @click.option(
     '-e',
     'evidence',
     metavar='EVIDENCE',
     default='',
     help='The evidence nodes, separated by commas. Default to empty set.')
-def app(graph_type, path_or_spec, node_a, node_b, evidence):
+@click.option(
+    '-m',
+    '--find-max-b-subset',
+    'max_subset',
+    is_flag=True,
+    default=False,
+    help='Print max independent subsets of NODES_B with NODES_A instead.',
+)
+def app(
+    path_or_spec: str,
+    nodes_a: str,
+    nodes_b: str,
+    evidence: str,
+    max_subset: bool,
+):
+    """
+    Test conditional independence between  NODE_A and NODE_B given EVIDENCE
+    in a Bayesian Network or a Markov Random Field. Print "yes" if they are
+    conditionally independent, "no" otherwise.
+    """
+    assert nodes_a and nodes_b
+    nodes_a = set(nodes_a.split(','))
+    nodes_b = set(nodes_b.split(','))
+    evidence = set(evidence.split(',') if evidence else [])
     spec = read_spec(path_or_spec)
-    evidence = evidence.split(',') if evidence else []
-    # Since we only need to test conditional independence, keeping the domain
-    # size (the 2nd argument to `gum.fastBN` or `gum.fastMRF`) as default value
-    # suffices.
-    g = gum.fastBN(spec) if graph_type == 'bn' else gum.fastMRF(spec)
-    if g.isIndependent(node_a, node_b, evidence):
-        click.echo('yes')
+
+    graph = spec_parser.build_graph(spec_parser.parse_to_edge_groups(spec))
+    if isinstance(graph, nx.DiGraph):
+        if not max_subset:
+            if bn.check_conditional_independence(graph, nodes_a, nodes_b,
+                                                 evidence):
+                click.echo('yes')
+            else:
+                click.echo('no')
+        else:
+            for sub_b in bn.search_max_independent_set(graph, nodes_a, nodes_b,
+                                                       evidence):
+                aug_evi = sorted(evidence | (nodes_b - set(sub_b)))
+                click.echo('{} ⊥ {} | {}'.format(
+                    pprint_nodes(nodes_a), pprint_nodes(sub_b),
+                    pprint_nodes(aug_evi)))
     else:
-        click.echo('no')
+        raise NotImplementedError('MRF graph separation not implemented yet')
