6a745afef2508560cd55692c44d716cc894767ce
kkew3 <kps6326@hotmail.com>
2024-10-23 23:04:41 +0800

    Set the prerelease version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='dsep',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.2.0-rc1',
     entry_points={
         'console_scripts': [
             'dsep = dsep.dsep:app',
